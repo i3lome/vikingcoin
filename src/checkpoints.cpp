71f44a23f1db166c544eb754a7dea0b9dcb165df
root <root@singleblade.info>
2014-06-13 22:31:07 -0400

    Check Point Added

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -38,6 +38,7 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
         ( 0,     uint256("0x00000c07c30a4f74683e64ed156668e3f8d2e71bb4efcdcc44c248708d1cabf2"))
+	( 13083,     uint256("0x000000000002d264461381aa93918273184d86a6fc46f5f236b20ccda1810758"))
 
         ;
     static const CCheckpointData data = {
