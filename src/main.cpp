8eddca35e466015d0d75170e47b0473f90f2a645
i3lome <i3lome@gmail.com>
2014-08-23 21:27:30 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2605,20 +2605,20 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CDiskBlockPos* dbp)
                                      REJECT_OBSOLETE, "bad-version");
             }
         }*/
-        // Enforce block.nVersion=2 rule that the coinbase starts with serialized block height
-        //if (block.nVersion >= 2)
-        //{
-        //    // if 750 of the last 1,000 blocks are version 2 or greater (51/100 if testnet):
-        //    if ((!TestNet() && CBlockIndex::IsSuperMajority(2, pindexPrev, 750, 1000)) ||
-        //        (TestNet() && CBlockIndex::IsSuperMajority(2, pindexPrev, 51, 100)))
-        //    {
-        //        CScript expect = CScript() << nHeight;
-        //        if (block.vtx[0].vin[0].scriptSig.size() < expect.size() ||
-        //            !std::equal(expect.begin(), expect.end(), block.vtx[0].vin[0].scriptSig.begin()))
-        //            return state.DoS(100, error("AcceptBlock() : block height mismatch in coinbase"),
-        //                             REJECT_INVALID, "bad-cb-height");
-        //    }
-        //}
+         Enforce block.nVersion=2 rule that the coinbase starts with serialized block height
+        if (block.nVersion >= 2)
+        {
+            // if 750 of the last 1,000 blocks are version 2 or greater (51/100 if testnet):
+            if ((!TestNet() && CBlockIndex::IsSuperMajority(2, pindexPrev, 750, 1000)) ||
+               (TestNet() && CBlockIndex::IsSuperMajority(2, pindexPrev, 51, 100)))
+            {
+                CScript expect = CScript() << nHeight;
+               if (block.vtx[0].vin[0].scriptSig.size() < expect.size() ||
+                    !std::equal(expect.begin(), expect.end(), block.vtx[0].vin[0].scriptSig.begin()))
+                    return state.DoS(100, error("AcceptBlock() : block height mismatch in coinbase"),
+                                     REJECT_INVALID, "bad-cb-height");
+            }
+        
     }
 
     // Write block to history file
