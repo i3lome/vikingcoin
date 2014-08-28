10e7c29120154241ef43424cf394214bf5a30068
i3lome <i3lome@gmail.com>
2014-08-27 22:38:16 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -475,7 +475,7 @@ unsigned int LimitOrphanTxSize(unsigned int nMaxOrphans)
 bool IsStandardTx(const CTransaction& tx, string& reason)
 {
     AssertLockHeld(cs_main);
-    if (tx.nVersion > CTransaction::CURRENT_VERSION || tx.nVersion < 2) {
+    if (tx.nVersion > CTransaction::CURRENT_VERSION || tx.nVersion < 1) {
         reason = "version";
         return false;
     }
